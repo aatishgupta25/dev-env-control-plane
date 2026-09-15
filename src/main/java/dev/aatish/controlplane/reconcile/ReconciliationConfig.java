918cdf79debcdab11f2e07121b66a42decb9f441
Aatish Gupta <156168433+aatishgupta25@users.noreply.github.com>
2026-09-15 01:17:10 -0400

    config: enable periodic reconciliation

---
+++ src/main/java/dev/aatish/controlplane/reconcile/ReconciliationConfig.java
@@ -0,0 +1,8 @@
+package dev.aatish.controlplane.reconcile;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ReconciliationConfig {}
